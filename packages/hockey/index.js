206f3b85439ce93e96db797ce2553040fd79fc9a
Douglas Wade <douglas.b.wade@gmail.com>
2024-01-21 23:01:15 -0800

    introduce a linting error to see the result on the pull request

--- packages/hockey/index.js
+++ packages/hockey/index.js
@@ -10,6 +10,12 @@ export default class MatchbotHockeyPlugin extends Plugin {
     }
     async preview() {
         this.logger.log('getting hockey preview');
+        
+        (async () => {
+            const response = await fetch('https://statsapi.web.nhl.com/api/v1/schedule?teamId=10&startDate=2021-01-01&endDate=2021-01-31');
+            const data = await response.json();
+            console.log(data);
+        }
 
         return '';
     }
