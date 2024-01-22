6816515d28af4d5e3d9bfca94fd8db8f83fe21f1
Douglas Wade <douglas.b.wade@gmail.com>
2024-01-21 16:09:45 -0800

    add sports-bot-specific config

--- packages/example/matchbot.js
+++ packages/example/matchbot.js
@@ -8,9 +8,24 @@ createBot({
     cache: 860000000,
     plugins: [
         {
-            community_id: 41717, // bot_test
-            config: { teamId: 2282, season: '2023' }, // Monterrey
+            community: 65003, // soundersfc
+            config: { teamId: 1595 },
             plugin: soccer,
-        }
+        },
+        {
+            community: 86091, // reign_fc
+            config: { teamId: 3002 },
+            plugin: soccer,
+        },
+        {
+            community: 86115, // ballard_fc
+            config: { teamId: 18882 },
+            plugin: soccer,
+        },
+        {
+            community: 5344, // seattlekraken
+            config: { teamId: 1436 },
+            plugin: hockey,
+        },
     ]
 });
